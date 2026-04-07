48201d722b70d1f39300d1e2915b61c455a430fc
lee <159456hl@gmail.com>
2026-04-06 23:28:06 -0400

    Initial Commit

---
+++ KineticTests/KineticTests.swift
@@ -0,0 +1,17 @@
+//
+//  KineticTests.swift
+//  KineticTests
+//
+//  Created by Music Loaner on 4/6/26.
+//
+
+import Testing
+@testable import Kinetic
+
+struct KineticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
